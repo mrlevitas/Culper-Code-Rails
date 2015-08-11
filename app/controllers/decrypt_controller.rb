1d4e4a8a1e56b95af5a033f6a790e1a0119a2576
mrlevitas <mrlevitas@gmail.com>
2015-08-11 16:17:55 -0500

    back to the start

--- app/controllers/decrypt_controller.rb
+++ app/controllers/decrypt_controller.rb
@@ -10,12 +10,12 @@ class DecryptController < ApplicationController
 	  @decrypt_text = Dmessage.new(decrypt_params) 
 
 	  puts "\n\n\n\nLOGS MAFUCKA!!"
-	  # p culper_code_hash = CulperDict.where(crypt: "decrypt").all.map(&:culper_hash)[0]
-	  culper_code_hash =  CulperDict.find_by_sql("SELECT * FROM culper_dicts WHERE crypt = 'decrypt' ")[0]
+	  culper_code_hash = CulperDict.where(crypt: "decrypt").all.map(&:culper_hash)[0]
+	  # culper_code_hash =  CulperDict.find_by_sql("SELECT * FROM culper_dicts WHERE crypt = 'decrypt' ")[0]
 		puts "balls 1 walls" + culper_code_hash.inspect
-		puts "\n\n\n\nLOGS MAFUCKA!!"
-		culper_code_hash_2 =  CulperDict.find_by_sql("SELECT * FROM culper_dicts WHERE crypt = 'decrypt'")
-		puts "balls 2 walls" + culper_code_hash_2.inspect
+		# puts "\n\n\n\nLOGS MAFUCKA!!"
+		# culper_code_hash_2 =  CulperDict.find_by_sql("SELECT * FROM culper_dicts WHERE crypt = 'decrypt'")
+		# puts "balls 2 walls" + culper_code_hash_2.inspect
 
 		decrypt_me_arr = @decrypt_text.decrypted.split(" ")
 
